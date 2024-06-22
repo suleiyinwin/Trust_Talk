9bf562bd9abaa8ad03944841ea01a55965681728
NayCMMKyaw <nayche.moh1@kmutt.ac.th>
2024-06-22 11:40:59 +0700

    [edit] markRead and badge in Navbar

--- backend/routers/chatRouter.js
+++ backend/routers/chatRouter.js
@@ -5,6 +5,7 @@ import expertinfo from '../controllers/chat/expertinfo.js';
 import chatlist from '../controllers/chat/chatlist.js';
 import getMessages from '../controllers/chat/getMessages.js';
 import userinfo from '../controllers/chat/getUserInfo.js';
+import {markRead} from '../controllers/chat/markRead.js';
 
 const chatRouter = Router();
 chatRouter.get('/expertlist',expertlist);
@@ -13,5 +14,6 @@ chatRouter.get('/expertinfo/:id', expertinfo);
 chatRouter.get('/chatlist', chatlist);
 chatRouter.get('/messages/:chatId', getMessages);
 chatRouter.get('/userinfo/:id', userinfo);
+chatRouter.post('/markread/:messageId', markRead);
 
 export default chatRouter;
